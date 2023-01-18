6957952c20950e5ee8a19b6551222999ac813dd6
ruanjiancheng <1434919953@qq.com>
2023-01-18 21:57:53 +0800

    interrupt

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "gdt.h"
 #include "printk.h"
+#include "interrupts.h"
 
 
 
@@ -19,9 +20,12 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
 {
     console_clear();
 
-    printk("hello waterkernel");
+    printk("hello waterkernel\n");
 
     GlobalDescriptorTable gdt;
+    InterruptManager interrupts(0x20, &gdt);
+
+    interrupts.Activate();
 
     while(1);
 }
